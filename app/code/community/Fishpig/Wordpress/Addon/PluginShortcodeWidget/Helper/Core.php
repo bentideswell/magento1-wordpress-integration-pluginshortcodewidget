451e92f0d60a505f1d17f5edd4ba5561480b0006
Ben Tideswell <ben@fishpig.co.uk>
2019-04-19 10:57:34 +0100

    1.0.0.38

--- app/code/community/Fishpig/Wordpress/Addon/PluginShortcodeWidget/Helper/Core.php
+++ app/code/community/Fishpig/Wordpress/Addon/PluginShortcodeWidget/Helper/Core.php
@@ -1,7 +1,8 @@
 <?php
-/**
- * @package Fishpig_Wordpress_Addon_PluginShortcodeWidget
- * @url     https://fishpig.co.uk/magento/wordpress-integration/
+/*
+ *
+ * @Obfuscate
+ *
  */
 class Fishpig_Wordpress_Addon_PluginShortcodeWidget_Helper_Core extends Mage_Core_Helper_Abstract
 {
@@ -84,6 +85,7 @@ class Fishpig_Wordpress_Addon_PluginShortcodeWidget_Helper_Core extends Mage_Cor
 			Mage::helper('wordpress');
 			Zend_Log::ERR;
 			Zend_Log_Formatter_Simple::DEFAULT_FORMAT;
+			Zend_Validate_File_Extension::NOT_FOUND;
 			class_exists('Zend_Log_Writer_Stream');
 
 			if (Mage::helper('wordpress')->isAddonInstalled('Multisite')) {
